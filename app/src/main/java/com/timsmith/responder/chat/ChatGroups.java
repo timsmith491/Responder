81bbc34cf48307812368d3b296bd707e8983d8b5
timsimth491 <timsmith491@gmail.com>
2017-04-17 11:29:42 +0100

    Chat functionality added

--- app/src/main/java/com/timsmith/responder/chat/ChatGroups.java
+++ app/src/main/java/com/timsmith/responder/chat/ChatGroups.java
@@ -2,18 +2,30 @@ package com.timsmith.responder.chat;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.timsmith.responder.R;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
 
 
 public class ChatGroups extends AppCompatActivity {
@@ -23,7 +35,7 @@ public class ChatGroups extends AppCompatActivity {
     private ListView listView;
     private ArrayAdapter<String> arrayAdapter;
     private ArrayList<String> list_of_rooms = new ArrayList<>();
-    private DatabaseReference mChatDatabase;
+    private DatabaseReference root = FirebaseDatabase.getInstance().getReference().getRoot();
     private String name;
 
 
@@ -43,20 +55,50 @@ public class ChatGroups extends AppCompatActivity {
 
         request_user_name();
 
-        mChatDatabase = FirebaseDatabase.getInstance().getReference().child("Chat");
-        mChatDatabase.keepSynced(true);
 
 
-//
-//        add_room.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Map<String, Object> map = new HashMap<String, Object>();
-//                map.put(room_name.getText().toString(), "");
-//                mChatDatabase.child("Chats").getRoot().updateChildren(map);
-//            }
-//        });
 
+        add_room.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Map<String, Object> map = new HashMap<String, Object>();
+                map.put(room_name.getText().toString(), "");
+                root.updateChildren(map);
+            }
+        });
+
+        root.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                Set<String> set = new HashSet<String>();
+                Iterator i = dataSnapshot.getChildren().iterator();
+
+                while (i.hasNext()){
+                    set.add(((DataSnapshot)i.next()).getKey());
+                }
+
+                list_of_rooms.clear();
+                list_of_rooms.addAll(set);
+                arrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                Intent intent = new Intent(getApplicationContext(),ChatRoom.class);
+                intent.putExtra("room_name",((TextView)view).getText().toString() );
+                intent.putExtra("user_name",name);
+                startActivity(intent);
+            }
+        });
     }
 
     private void request_user_name(){
